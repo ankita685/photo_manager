f84dfbfb2bdbdba6e31e732a4bed4ae3168cd67a
Ankita choudhary <99496688+ankita685@users.noreply.github.com>
2024-07-17 17:26:47 +0530

    Update FirebaseConfig.jsx

--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -7,14 +7,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
  
 
-  apiKey: "AIzaSyAihNwhPGATaghrv0uG6EoJwvV0rX19BUA",
-  authDomain: "task-ccf32.firebaseapp.com",
-  projectId: "task-ccf32",
-  databaseURL: "https://task-ccf32-default-rtdb.firebaseio.com/",
-  storageBucket: "task-ccf32.appspot.com",
-  messagingSenderId: "146201835486",
-  appId: "1:146201835486:web:8fc0ee339e231e05714f4f",
-  measurementId: "G-97TEWRZ44K"
+  //add ur firebase config
 };
  
 const app = initializeApp(firebaseConfig);
